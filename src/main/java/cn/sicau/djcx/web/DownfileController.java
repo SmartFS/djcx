f3e55d3f180290f94ea062c26c523e0bfb7cfc82
SmartFS <2452951224@qq.com>
2019-05-06 20:11:48 +0800

    数据库
    改变了数据库地址

--- src/main/java/cn/sicau/djcx/web/DownfileController.java
+++ src/main/java/cn/sicau/djcx/web/DownfileController.java
@@ -1,9 +1,14 @@
 package cn.sicau.djcx.web;
 
 
+import cn.sicau.djcx.entity.Downfile;
+import cn.sicau.djcx.mapper.DownfileMapper;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 import org.springframework.stereotype.Controller;
+
+import javax.annotation.Resource;
 
 /**
  * <p>
@@ -17,5 +22,13 @@ import org.springframework.stereotype.Controller;
 @RequestMapping("/downfile")
 public class DownfileController {
 
+    @Resource
+    DownfileMapper downfileMapper;
+    public void insertFile(){
+        Downfile downfile = new Downfile();
+        downfile.setFileId("fangfna");
+        downfile.setFileUrl("shusa");
+        downfileMapper.insert(downfile);
+    }
 }
 
